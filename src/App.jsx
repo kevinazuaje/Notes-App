58ef0f4b42e089a7a7cc8926afcfa18027b76049
kevin azuaje <kazuaje6@gmail.com>
2020-06-09 18:38:27 -0400

    components en carpeta individual

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,13 @@ import React, { Component } from "react";
 import "./App.css";
 import tasks from "./sample/tasks.json";
 import Tasks from "./components/Tasks.jsx";
-console.log(tasks);
 
 class App extends Component {
   state = {
     tasks: tasks,
   };
   render() {
-    return (
-      <div>
-        {this.state.tasks.map((e) => (
-          <p key={e.id}>
-            {e.title} - {e.description} - {e.done} - {e.priority} <Tasks />
-          </p>
-        ))}
-      </div>
-    );
+    return <div>{<Tasks tasks={this.state.tasks} />}</div>;
   }
 }
 
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,5 +1,15 @@
-import React, { component } from "react";
+import React, { Component } from "react";
 
-const Tasks = () => <h1>hellow world</h1>;
+class Tasks extends React.Component {
+  render() {
+    return this.props.tasks.map((e) => (
+      <p key={e.id}>
+        {e.title}--{e.description}--{e.done}-- {e.priority}
+        <input type="checkbox" />
+        <button id="button">x</button>
+      </p>
+    ));
+  }
+}
 
 export default Tasks;
