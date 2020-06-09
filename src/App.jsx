fe002e25ed1e7916f4bb84b6257ecb293e172a01
kevin azuaje <kazuaje6@gmail.com>
2020-06-09 17:17:31 -0400

    primera fase de  kapp

--- src/App.jsx
+++ src/App.jsx
@@ -1,99 +1,24 @@
-import React, { useState, useEffect } from "react";
+import React, { Component } from "react";
 import "./App.css";
+import tasks from "./sample/tasks.json";
+import Tasks from "./components/Tasks.jsx";
+console.log(tasks);
 
-// function Helloworld (props){
-//   console.log(props)
-//   return(
-
-//     <div id="hello">
-//        <h3>{props.subtitle}</h3>
-//     {props.text}</div>
-//   )
-// }
-/////////////////
-const Helloworld = (props) => {
-  const [show, setShow] = useState(true);
-
-  const toggleshow = () => {
-    setShow(!show);
+class App extends Component {
+  state = {
+    tasks: tasks,
   };
-
-  useEffect(() => {
-    console.log("me monte");
-
-    return () => {
-      console.log("chao");
-    };
-  }, []);
-
-  useEffect(() => {
-    console.log("cambie el show", show);
-  }, [show]);
-
-  if (show) {
+  render() {
     return (
-      <div id="hello">
-        <h3>{props.subtitle}</h3>
-        {props.text}
-        <button onClick={toggleshow}>toggle Show </button>
+      <div>
+        {this.state.tasks.map((e) => (
+          <p key={e.id}>
+            {e.title} - {e.description} - {e.done} - {e.priority} <Tasks />
+          </p>
+        ))}
       </div>
     );
   }
-  return (
-    <div>
-      <h1>property are not defined</h1>
-      <button onClick={toggleshow}>toggle Show </button>
-    </div>
-  );
-};
-///////////////////
-// class Helloworld extends React.Component {
-//   state = {
-//     show: true,
-//   };
-
-//   toggleshow = () => {
-//     this.setState({ show: !this.state.show });
-//   };
-
-//   componentWillMount() {
-//     console.log("voy a montarme");
-//   }
-//   componentDidMount() {
-//     console.log("montado");
-//   }
-//   render() {
-//     if (this.state.show) {
-//       return (
-//         <div id="hello">
-//           <h3>{this.props.subtitle}</h3>
-//           {this.props.text}
-//           <button onClick={this.toggleshow}>toggle Show </button>
-//         </div>
-//       );
-//     } else {
-//       return (
-//         <div>
-//           <h1>this property are not defined</h1>
-//           <button onClick={this.toggleshow}>toggle Show </button>
-//         </div>
-//       );
-//     }
-//   }
-// }
-
-const App = () => {
-  const [showApp, setShowApp] = useState(true);
-
-  return (
-    <div className="App">
-      this is my component:{" "}
-      {showApp && <Helloworld text="hola kevin" subtitle="hey" />}
-      <button onClick={() => setShowApp(!showApp)}>toggle Show app </button>
-      {/* <Helloworld text="hola mundo" subtitle="que hay" />
-      <Helloworld text="Hello!" subtitle="Ingles" /> */}
-    </div>
-  );
-};
+}
 
 export default App;
---
+++ src/components/Tasks.jsx
@@ -0,0 +1,5 @@
+import React, { component } from "react";
+
+const Tasks = () => <h1>hellow world</h1>;
+
+export default Tasks;
